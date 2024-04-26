6ed8e761e3b31af07df162ab0d9fcf79f38fd232
Sofian Slimani <70762272+Sev7n2507@users.noreply.github.com>
2024-04-26 20:25:07 +0200

    refactor: update WindowContext with enhanced structure and add gitignore rules

--- src/js/utils/device/DeviceAcceleration.js
+++ src/js/utils/device/DeviceAcceleration.js
@@ -1,43 +1,45 @@
-import { Accelerometer } from "../../libs/sensor-polyfills/src/motion-sensors"
-import EventEmitter from "../events/EventEmitter"
+import { Accelerometer } from "../../libs/sensor-polyfills/src/motion-sensors";
+import EventEmitter from "../events/EventEmitter";
 
 export default class DeviceAcceleration extends EventEmitter {
-    constructor() {
-        super()
+  constructor() {
+    super();
 
-        /** coordinates */
-        this.x = 0
-        this.y = 0
-        this.z = 0
+    /** coordinates */
+    this.x = 0;
+    this.y = 0;
+    this.z = 0;
 
-        /** permission */
-        if (navigator.permissions) {
-            Promise.all([navigator.permissions.query({ name: "accelerometer" })])
-                .then(results => {
-                    if (results.every(
-                        result => result.state === "granted")) {
-                        this.init()
-                    } else {
-                        console.log("Permission to use sensor was denied.")
-                    }
-                }).catch(err => {
-                    console.log("Integration with Permissions API is not enabled, still try to start app.")
-                    this.init()
-                })
-        } else {
-            console.log("No Permissions API, still try to start app.")
-            this.init()
-        }
-    }
-
-    init() {
-        this.sensor = new Accelerometer({ frequency: 60 })
-        this.sensor.addEventListener('reading', () => {
-            this.x = this.sensor.x
-            this.y = this.sensor.y
-            this.z = this.sensor.z
-            this.trigger('reading')
+    /** permission */
+    if (navigator.permissions) {
+      Promise.all([navigator.permissions.query({ name: "accelerometer" })])
+        .then((results) => {
+          if (results.every((result) => result.state === "granted")) {
+            this.init();
+          } else {
+            console.log("Permission to use sensor was denied.");
+          }
         })
-        this.sensor.start()
+        .catch((err) => {
+          console.log(
+            "Integration with Permissions API is not enabled, still try to start app."
+          );
+          this.init();
+        });
+    } else {
+      console.log("No Permissions API, still try to start app.");
+      this.init();
     }
+  }
+
+  init() {
+    this.sensor = new Accelerometer({ frequency: 60 });
+    this.sensor.addEventListener("reading", () => {
+      this.x = this.sensor.x;
+      this.y = this.sensor.y;
+      this.z = this.sensor.z;
+      this.trigger("reading");
+    });
+    this.sensor.start();
+  }
 }
--- src/main.js
+++ src/main.js
@@ -1,58 +1,63 @@
-import * as THREE from "three"
-import WindowContext from "./js/WindowContext"
-import SceneBouncingBubbles from "./js/scenarios/BouncingBubbles/SceneBouncingBubbles"
-import { askMotionAccess } from "./js/utils/device/DeviceAccess"
-import SceneScenario3D from "./js/scenarios/Scenario3D/SceneScenario3D"
+import * as THREE from "three";
+import WindowContext from "./js/WindowContext";
+import SceneBouncingBubbles from "./js/scenarios/BouncingBubbles/SceneBouncingBubbles";
+import { askMotionAccess } from "./js/utils/device/DeviceAccess";
+import SceneScenario3D from "./js/scenarios/Scenario3D/SceneScenario3D";
 
 /** device access */
-const btn = document.getElementById("btn-access")
-btn.addEventListener("click", askMotionAccess(), false)
+const btn = document.getElementById("btn-access");
+btn.addEventListener("click", askMotionAccess(), false);
 
 /** reload */
-const btnReload = document.getElementById("btn-reload")
-btnReload.addEventListener("click", function () {
-    window.location.reload()
-}, false)
+const btnReload = document.getElementById("btn-reload");
+btnReload.addEventListener(
+  "click",
+  function () {
+    window.location.reload();
+  },
+  false
+);
 
 /** scenarios */
-const scene1 = new SceneBouncingBubbles(20)
-const scene2 = new SceneScenario3D("canvas-scene-3d")
-const scene3 = new SceneBouncingBubbles(10, "canvas-scene-2")
+const scene1 = new SceneBouncingBubbles(20);
+const scene2 = new SceneScenario3D("canvas-scene-3d");
+const scene3 = new SceneBouncingBubbles(10, "canvas-scene-2");
 
-const windowContext = new WindowContext()
-console.log(windowContext.scenes)
-const time = windowContext.time
+const windowContext = new WindowContext();
+console.log(windowContext.scenes);
+const time = windowContext.time;
 
 const update = () => {
-    /** example */
-    scene1.bubbles.forEach(b => {
-        if (b.x < scene1.width / 2) {
-            b.radius = 20
-        } else {
-            b.radius = 5
-        }
-    })
+  /** example */
+  scene1.bubbles.forEach((b) => {
+    if (b.x < scene1.width / 2) {
+      b.radius = 20;
+    } else {
+      b.radius = 5;
+    }
+  });
 
-    /** 1 -> check des bulles dans les différents scénarios */
-    const outFromScene1 = scene1.bubbles.filter(b => { return b.x > scene1.width / 2 }) // remove bubbles
-    const outFromScene2 = scene2.bubbles.filter(b => { return b.x > scene1.width / 2 }) // remove bubbles
+  /** 1 -> check des bulles dans les différents scénarios */
+  const outFromScene1 = scene1.bubbles.filter((b) => {
+    return b.x > scene1.width / 2;
+  }); // remove bubbles
+  const outFromScene2 = scene2.bubbles.filter((b) => {
+    return b.x > scene1.width / 2;
+  }); // remove bubbles
 
-    /** 2 -> mise à jour des scénarios */
-    outFromScene1.forEach(b => {
-        // 1 - remove from scene 1 = update scene1.bubbles
+  /** 2 -> mise à jour des scénarios */
+  outFromScene1.forEach((b) => {
+    // 1 - remove from scene 1 = update scene1.bubbles
+    // 2 - add to other scene => sceneX.addBubble
+    // b.vx
+  });
+  outFromScene2.forEach((b) => {
+    // 1 - remove from scene 2 = removeBubble(b)
+    // 2 - add to other scene => sceneX.addBubble
+    // b.vx
+  });
 
-        // 2 - add to other scene => sceneX.addBubble
-        // b.vx
-    })
-    outFromScene2.forEach(b => {
-        // 1 - remove from scene 2 = removeBubble(b)
+  console.log(outFromScene1);
+};
 
-        // 2 - add to other scene => sceneX.addBubble
-        // b.vx
-    })
-
-    console.log(outFromScene1)
-}
-
-time.on('update', update)
-
+time.on("update", update);
